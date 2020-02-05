94afcae2fa77a8766f7f6e06b69044080ef7484d
Nhan Thanh Vo <48186215+Talonss5@users.noreply.github.com>
2020-02-05 11:13:52 -0800

    Add files via upload

---
+++ Lab 5-2.py
@@ -0,0 +1,10 @@
+#Nhan Vo
+#Part  Problem 2:
+def main():
+    get_input()
+    display_output()
+def get_input():
+    name='Marry'
+def display_output():
+    print('Your name is',name)
+main()
---
+++ Lab 5-3.py
@@ -0,0 +1,6 @@
+#Nhan Vo
+#Part  Problem 3:
+for i in range (3):
+    for j in range (0,4,2):
+        print(i,j, end=';')
+    
